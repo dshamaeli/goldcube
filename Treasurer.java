e866db04303057e14ba0a8f6ef2d7eee664fc038
Daniel <daniel.shamaeli@gmail.com>
2017-10-02 01:12:27 +0100

    Treasurer is brokern :(

---
+++ Treasurer.java
@@ -0,0 +1,42 @@
+/** Author:Daniel Shamaeli, Student ID:201230464
+ *  *  first assigment for COMP517*/
+
+//impoert package to read data
+
+import goldCube.GoldCube;
+
+import java.util.Scanner;
+
+public class Treasurer
+{
+	private Scanner input = new Scanner(System.in);
+	
+	private GoldCube[] cub = new GoldCube[3];
+	//boolean answer;
+	
+	//main methods
+	public static void main(String[] args)
+	{
+		int i;
+
+		for(i=0 ; i<3 ; i++)
+		{
+			System.out.println("Please enter Size for cube number " + i + ":");
+			cube[i] = new GoldCube();
+			cube[i].size = input.nextfloat();
+			
+			System.out.println("Please enter price for cube number " + i + ":");
+			cube[i].price = input.nextfloat();
+
+			//implement user prompt to continue
+		}
+		System.out.println("The Result is");
+
+		for( i=o ; i<3 ; i++)
+		{
+			System.out.println("GoldCube No." + i + " Volume: " + cube[i].volume() +
+					" Price: " + cube[i].value);
+		}
+	}	
+
+}
