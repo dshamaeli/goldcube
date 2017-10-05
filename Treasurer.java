b9421f121b798ef67235dc235dc11ba36a64295e
Daniel <daniel.shamaeli@gmail.com>
2017-10-05 22:36:08 +0100

    impelement validator method and enhanced the output

--- Treasurer.java
+++ Treasurer.java
@@ -14,48 +14,65 @@ public class Treasurer
 
 	private static float size;
 	private static float price;
+	
+	public static float validFloat(String tag,int counter)
+	{
+		float var;
+		Scanner input = new Scanner(System.in);
+
+		do
+		{   
+			//prompting user for gold cube price
+			System.out.println("Please enter "+ tag +" for cube No." + counter + " :");
+				
+			//check an see if input is a number
+			while(!input.hasNextFloat())  
+			{   
+				System.out.println("The gold-cube " + tag +" should be a positive number");
+				input.next();
+			}
+
+			//geting use input
+			var = input.nextFloat();
+				
+		//check if input is positive	
+		}while(var <=0);  
+
+		return var;
+	}
+
 	//main methods */
 	public static void main(String[] args)
 	{
-		Scanner input = new Scanner(System.in);
-	    GoldCube[] goldCube = new GoldCube[3];
+		//container for goldCubes
+		GoldCube[] goldCube = new GoldCube[3];
 
-		//this loop will get the size and price and sets them to the cube
+		//container for price of goldCubes
+		float price;
+
+		//container for size of goldCubes
+		float size;
+
+		//this loop will get the size and price and sets them to the goldCubes
 		for(int i=0, j = 1 ; i<3 ; i++, j++)
 		{	
+			//get a goldCube price
+			price = validFloat("price",j);
 			
-			//validating user input
-			do
-			{   
-				//prompting user for gold cube price
-				System.out.println("Please enter price for cube number " + j + "(per cc):");
-				
-				//check an see if input is a number
-				while(!input.hasNextFloat())  
-				{   
-					System.out.println("the price should be a positive number");
-					input.next();
-				}
+			//get a goldCube size
+			size = validFloat("size",j);
 
-				//geting use input
-				price = input.nextFloat();
-				
-			}while(price <=0); 
-			
-			System.out.println("Please enter Size cube number "+ j + "(cm):");
-			//validating user input	
-			size = input.nextFloat();
-		
+			//Ceate a goldCube with given price and size
 			goldCube[i] = new GoldCube(size,price);
 
-			//implement user prompt to continue
+			//print result for user
 		}
-		System.out.println("The Result is");
 
 		for(int i = 0 ,j = 1 ; i<3 ; i++, j++)
 		{
-			System.out.println("GoldCube No." + j + " Volume: " + goldCube[i].volume()
-			 + " Price: " + goldCube[i].value());
-		}
+			System.out.println("**************************");
+			System.out.println("GoldCube No." + j + "\nVolume: " + goldCube[i].volume() +" cc"
+			 + "\nPrice: " + goldCube[i].value()+" £\n");
+					}
 	}
 }
